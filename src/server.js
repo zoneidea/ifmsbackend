8218d87042c82132533e7cd9a91f051ca418e06a
zoneidea <dev@zoneidea.co.th>
2026-03-31 15:53:30 +0700

    Add allow origin cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,24 @@ require("dotenv").config();
 const app = require("./app");
 const { getPool } = require("./db");
 
-const PORT = Number(process.env.PORT || 3000);
+const app = express();
+app.use(express.json());
 
+const whiteList = ['localhost:3000', 'http://localhost:3000', 'http://localhost:8080'];
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whiteList.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }
+    } else {
+        corsOptions = { origin: false }
+    }
+
+    callback(null, corsOptions)
+}
+
+app.use(cors(corsOptionsDelegate));
+
+const PORT = Number(process.env.PORT || 3000);
 async function start() {
     try {
         await getPool();
