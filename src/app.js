c680f03ba1194864809461618ed4822f0e60cbd6
zoneidea <dev@zoneidea.co.th>
2026-04-07 17:25:01 +0700

    Bind IP

--- src/app.js
+++ src/app.js
@@ -8,30 +8,33 @@ const customerRoutes = require("./modules/customers/customer.routes");
 const usersRoutes = require("./modules/users/user.routes")
 const customerReportRoutes = require('./modules/customerreports/customerreport.routes')
 
+const reportRoutes = require("./modules/reports/report.routes");
+const connectionRoutes = require("./modules/connections/connection.routes");
+
 const app = express();
 app.set("trust proxy", false);
 
 app.use(helmet());
-app.use(express.json({ limit: "1024mb" }));
+app.use(express.json({ limit: "10mb" }));
 
 const whiteList = ['http://localhost:8080', 'http://141.98.17.228:8080'];
-var corsOptionsDelegate = function (req, callback) {
-    var corsOptions;
-    if (whiteList.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true }
-    } else {
-        corsOptions = { origin: false }
-    }
+const corsOptionsDelegate = (req, callback) => {
+    const origin = req.header('Origin');
+    const isWhitelisted = whiteList.includes(origin);
 
-    callback(null, corsOptions)
-}
-
+    callback(null, {
+        origin: isWhitelisted,
+        credentials: true
+    });
+};
 app.use(cors(corsOptionsDelegate));
 
 app.use(
     rateLimit({
         windowMs: 15 * 60 * 1000,
-        max: 100,
+        max: 500,
+        standardHeaders: true,
+        legacyHeaders: false,
         validate: { trustProxy: false }, // ปิดการตรวจสอบแจ้งเตือนนี้
     })
 );
@@ -40,16 +43,12 @@ app.get("/health", (req, res) => {
     res.json({ ok: true, ts: new Date().toISOString() });
 });
 
-const connectionRoutes = require("./modules/connections/connection.routes");
+
 app.use("/connections", connectionRoutes);
-
-const reportRoutes = require("./modules/reports/report.routes");
 app.use("/reports", reportRoutes);
-
 // ✅ POST /customers
 app.use("/customers", customerRoutes);
 app.use('/users', usersRoutes)
-
 app.use('/ExternalReports', customerReportRoutes);
 
 // ✅ error handler ต้องท้ายสุด
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ async function start() {
         await getPool();
         console.log("✅ MSSQL connected");
 
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`🚀 Server running on http://localhost:${PORT}`);
         });
     } catch (err) {
